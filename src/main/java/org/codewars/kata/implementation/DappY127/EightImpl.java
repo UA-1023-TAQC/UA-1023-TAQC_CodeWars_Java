1c98221c7139bfde923832f838429419f04c0219
Paul <paul.mshtk.707@gmail.com>
2023-06-12 12:16:23 +0300

    add KeepHydratet kata solution

--- src/main/java/org/codewars/kata/implementation/DappY127/EightImpl.java
+++ src/main/java/org/codewars/kata/implementation/DappY127/EightImpl.java
@@ -4,7 +4,8 @@ import org.codewars.kata.Eight;
 
 public class EightImpl implements Eight {
     public int liters(double time) {
-        return 0;
+        //KeepHydrated
+        return (int) (time * 0.5);
     }
 
     public double getVolumeOfCuboid(double length, double width, double height) {
