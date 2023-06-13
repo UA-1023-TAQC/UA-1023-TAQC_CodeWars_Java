d0a110412bef9dd264d4919dc0e689116c93602d
Valeriia Moskalenko <moskalenkovvy@gmail.com>
2023-06-13 21:08:27 +0300

    add variance solution

--- src/main/java/org/codewars/kata/implementation/mosvvy/SixImpl.java
+++ src/main/java/org/codewars/kata/implementation/mosvvy/SixImpl.java
@@ -2,6 +2,8 @@ package org.codewars.kata.implementation.mosvvy;
 
 import org.codewars.kata.Six;
 
+import java.util.Arrays;
+
 public class SixImpl implements Six {
     public long findNb(long m) {
         return 0;
@@ -20,7 +22,22 @@ public class SixImpl implements Six {
     }
 
     public double variance(String town, String strng) {
-        return 0;
+        String[] str = strng.split("\n");
+        double result = -1.0;
+        for (String s : str) {
+            if (s.indexOf(town+":") == 0) {
+                double[] vals = Arrays.stream(s.replaceAll("[^0-9.,]", " ").split(","))
+                        .mapToDouble(Double::parseDouble).toArray();
+                double med = Arrays.stream(vals).sum() / vals.length;
+                result = 0.0;
+                for (double val : vals) {
+                    result += Math.pow(val - med, 2);
+                }
+                result /= vals.length;
+                break;
+            }
+        }
+        return result;
     }
 
     public String nbaCup(String resultSheet, String toFind) {
