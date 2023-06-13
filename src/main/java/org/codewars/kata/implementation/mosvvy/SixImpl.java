076c68fc7dc551a3e6aa7510b6986b5407ecfe75
Valeriia Moskalenko <moskalenkovvy@gmail.com>
2023-06-13 21:07:33 +0300

    add mean solution

--- src/main/java/org/codewars/kata/implementation/mosvvy/SixImpl.java
+++ src/main/java/org/codewars/kata/implementation/mosvvy/SixImpl.java
@@ -16,7 +16,20 @@ public class SixImpl implements Six {
     }
 
     public double mean(String town, String strng) {
-        return 0;
+        String[] str = strng.split("\n");
+        double result = -1.0;
+        for (String s : str) {
+            if (s.indexOf(town+":") == 0) {
+                String[] values = s.replaceAll("[^0-9.,]", " ").split(",");
+                result = 0.0;
+                for (String value : values) {
+                    result += Double.parseDouble(value);
+                }
+                result /= 12;
+                break;
+            }
+        }
+        return result;
     }
 
     public double variance(String town, String strng) {
