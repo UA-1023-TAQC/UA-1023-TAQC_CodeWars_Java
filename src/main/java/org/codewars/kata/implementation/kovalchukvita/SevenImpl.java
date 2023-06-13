435147c2ac8c3287845ff4f045a142610ccc2c0e
Kovalchuk_Vita <vita.kovalchuk06@gmail.com>
2023-06-13 09:26:13 +0300

    add solutions Codewars

--- src/main/java/org/codewars/kata/implementation/kovalchukvita/SevenImpl.java
+++ src/main/java/org/codewars/kata/implementation/kovalchukvita/SevenImpl.java
@@ -4,10 +4,23 @@ import org.codewars.kata.Seven;
 
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        double numDonations = arr.length + 1;
+        double expectedDonation = navg * numDonations - sum;
+
+        if (expectedDonation <= 0) {
+            throw new IllegalArgumentException();
+        }
+        return (long) Math.ceil(expectedDonation);
     }
 
     public String seriesSum(int n) {
-        return null;
+        double sum = 0d;
+        for (int i = 0; i < n; i++)
+            sum += 1d / (1 + 3 * i);
+        return String.format("%.2f", sum);
     }
 }
