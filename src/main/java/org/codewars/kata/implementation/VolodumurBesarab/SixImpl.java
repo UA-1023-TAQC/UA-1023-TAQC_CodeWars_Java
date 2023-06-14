727552544fa0cc6faa812d2ea693e6045dfac39a
Volodumur Besarab <volodumur911@gmail.com>
2023-06-14 13:17:00 +0300

    Add balance

--- src/main/java/org/codewars/kata/implementation/VolodumurBesarab/SixImpl.java
+++ src/main/java/org/codewars/kata/implementation/VolodumurBesarab/SixImpl.java
@@ -2,13 +2,57 @@ package org.codewars.kata.implementation.VolodumurBesarab;
 
 import org.codewars.kata.Six;
 
+import java.text.DecimalFormat;
+
 public class SixImpl implements Six {
     public long findNb(long m) {
         return 0;
     }
 
-    public String balance(String book) {
-        return null;
+    public String balance(String book)
+    {
+        if (book == null || book == "")
+            return "";
+
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+
+        book = book.replaceAll("!","");
+        book = book.replaceAll("=","");
+        book = book.replaceAll(":","");
+        book = book.replaceAll("\\?","");
+        book = book.replaceAll(";","");
+        book = book.replaceAll("\\{","");
+        book = book.replaceAll("}","");
+        String result = "";
+        double totalExpense = 0;
+        String[] lines = book.split("\n");
+        Double balance = Double.parseDouble(lines[0]);
+        result += "Original Balance: " + lines[0] + "\\r\\n";
+        lines[0] = null;
+        for (var line:lines) {
+            if (line != null)
+            {
+                String dataLine;
+                char lastSymbol = line.charAt(line.length()-1);
+                if (lastSymbol == ' ')
+                    dataLine = line.substring(0,line.length()-1);
+                else
+                    dataLine = line;
+                int indexOfLastSpace = dataLine.lastIndexOf(' ');
+                int indexOfDot = dataLine.indexOf('.');
+                Double checkAmount = Double.valueOf(dataLine.substring(indexOfLastSpace, indexOfDot+3));
+                balance -=checkAmount;
+                totalExpense += checkAmount;
+                int cutLine = dataLine.indexOf(' ');
+                while (line.charAt(cutLine + 1) != ' ')
+                    cutLine++;
+                result += line.substring(0,cutLine + 1) + " " + decimalFormat.format(checkAmount) +" Balance " + String.format("%.2f", balance) +"\\r\\n";
+            }
+        }
+        result += "Total expense  " + String.format("%.2f", totalExpense) + "\\r\\n";
+        result += "Average expense  "  + String.format("%.2f", totalExpense/(lines.length-1));
+        result = result.replaceAll(",",".");
+        return result;
     }
 
     public double f(double x) {
