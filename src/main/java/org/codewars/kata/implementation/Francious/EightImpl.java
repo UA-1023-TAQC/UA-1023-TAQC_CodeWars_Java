857f2c3cd628cbef8bc086ab803339ab7c3c1250
Rostyslav <rostik.krotenko@gmail.com>
2023-06-14 23:13:33 +0300

    solved balance

--- src/main/java/org/codewars/kata/implementation/Francious/EightImpl.java
+++ src/main/java/org/codewars/kata/implementation/Francious/EightImpl.java
@@ -1,75 +1,38 @@
 package org.codewars.kata.implementation.Francious;
 
 import org.codewars.kata.Eight;
-import java.util.ArrayList;
 
 public class EightImpl implements Eight {
     public int liters(double time) {
-        return (int) Math.floor(time * 0.5);
+        return 0;
     }
 
     public double getVolumeOfCuboid(double length, double width, double height) {
-        return length * width * height;
+        return 0;
     }
 
     public float mpgToKPM(float mpg) {
-        double gallonPerMile = 4.54609188 / 1.609344;
-        double result = mpg / gallonPerMile;
-        return (float) Math.round(result * 100.0) / 100;
+        return 0;
     }
 
     public int[] squareOrSquareRoot(int[] array) {
-        for (int i = 0; i < array.length; i++){
-            double sqrt = Math.sqrt(array[i]);
-            if (sqrt == (int) sqrt) {
-                array[i] = (int) sqrt;
-            } else {
-                array[i] *= array[i];
-            }
-        }
-        return array;
+        return new int[0];
     }
 
     public int[] countPositivesSumNegatives(int[] input) {
-        if (input == null || input.length == 0) {
-            return new int[0];
-        }
-
-        int[] result = new int[2];
-
-        for (int i = 0; i < input.length; i++){
-            if (input[i] > 0){
-                result[0] += 1;
-            }
-            if (input[i] < 0) {
-                result[1] += input[i];
-            }
-        }
-        return result;
+        return new int[0];
     }
 
     public int stringToNumber(String str) {
-        return Integer.parseInt(str);
+        return 0;
     }
 
     public double TwoDecimalPlaces(double number) {
-
-        return Math.round(number * 100.00) / 100.00;
+        return 0;
     }
 
     public int[] divisibleBy(int[] numbers, int divider) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < numbers.length; i++){
-            if (numbers[i] % divider == 0){
-                list.add(numbers[i]);
-            }
-        }
-        int[] result = new int[list.size()];
-        for (int j = 0; j < list.size(); j++){
-            result[j] = list.get(j);
-        }
-
-        return result;
+        return new int[0];
     }
 
     @Override
--- src/main/java/org/codewars/kata/implementation/Francious/SevenImpl.java
+++ src/main/java/org/codewars/kata/implementation/Francious/SevenImpl.java
@@ -1,40 +1,14 @@
 package org.codewars.kata.implementation.Francious;
 
 import org.codewars.kata.Seven;
-import java.text.DecimalFormat;
+
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
-        double result = 0;
-        for (double donation : arr){
-            result += donation;
-        }
-        double length = arr.length;
-        double exptResult = ((length + 1) *  navg - result);
-
-        if (exptResult <= 0) {
-            throw new IllegalArgumentException();
-        }
-
-        return (long) Math.ceil(exptResult);
+        return 0;
     }
 
     public String seriesSum(int n) {
-        DecimalFormat decimalFormat = new DecimalFormat("0.00");
-
-        if(n <= 0) {
-            return new String("0.00");
-        }
-
-        double result = 0;
-        double denominator = 1;
-        for (int i = 0; i < n; i++) {
-            result += 1 / denominator;
-            denominator += 3;
-        }
-
-        result = Math.round(result * 100);
-        String resultString = String.format("%.2f", result / 100);
-        return resultString;
+        return null;
     }
 
     @Override
