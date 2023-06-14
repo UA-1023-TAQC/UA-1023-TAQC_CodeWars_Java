96e64e2011f28be0450da06f5a73de0d6b77de2e
Rostyslav <rostik.krotenko@gmail.com>
2023-06-14 18:06:12 +0300

    add 17 solved tasks

--- src/main/java/org/codewars/kata/implementation/Francious/SevenImpl.java
+++ src/main/java/org/codewars/kata/implementation/Francious/SevenImpl.java
@@ -1,13 +1,39 @@
 package org.codewars.kata.implementation.Francious;
 
 import org.codewars.kata.Seven;
-
+import java.text.DecimalFormat;
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double result = 0;
+        for (double donation : arr){
+            result += donation;
+        }
+        double length = arr.length;
+        double exptResult = ((length + 1) *  navg - result);
+
+        if (exptResult <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return (long) Math.ceil(exptResult);
     }
 
     public String seriesSum(int n) {
-        return null;
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        if(n <= 0) {
+            return new String("0.00");
+        }
+
+        double result = 0;
+        double denominator = 1;
+        for (int i = 0; i < n; i++) {
+            result += 1 / denominator;
+            denominator += 3;
+        }
+
+        result = Math.round(result * 100);
+        String resultString = String.format("%.2f", result / 100);
+        return resultString;
     }
 }
