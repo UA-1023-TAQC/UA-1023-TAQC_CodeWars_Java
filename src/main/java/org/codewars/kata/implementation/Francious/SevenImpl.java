ae00b409b61a1a3501471788f915823ff96484fa
Rostyslav <rostik.krotenko@gmail.com>
2023-06-14 22:31:12 +0300

    solved newAvg

--- src/main/java/org/codewars/kata/implementation/Francious/SevenImpl.java
+++ src/main/java/org/codewars/kata/implementation/Francious/SevenImpl.java
@@ -4,7 +4,18 @@ import org.codewars.kata.Seven;
 
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double result = 0;
+        for (double donation : arr){
+            result += donation;
+        }
+        double length = arr.length;
+        double exptResult = ((length + 1) *  navg - result);
+
+        if (exptResult <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return (long) Math.ceil(exptResult);
     }
 
     public String seriesSum(int n) {
