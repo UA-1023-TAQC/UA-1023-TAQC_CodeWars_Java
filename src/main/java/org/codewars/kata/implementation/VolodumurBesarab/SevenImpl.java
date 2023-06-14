bd4a4bf70a673fdde5c42bf57829e762a4567bf2
Volodumur Besarab <volodumur911@gmail.com>
2023-06-14 12:56:49 +0300

    Add seriesSum

--- src/main/java/org/codewars/kata/implementation/VolodumurBesarab/SevenImpl.java
+++ src/main/java/org/codewars/kata/implementation/VolodumurBesarab/SevenImpl.java
@@ -2,13 +2,28 @@ package org.codewars.kata.implementation.VolodumurBesarab;
 
 import org.codewars.kata.Seven;
 
+import java.text.DecimalFormat;
+
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
         return 0;
     }
 
-    public String seriesSum(int n) {
-        return null;
+    public String seriesSum(int n)
+    {
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+        if (n == 0)
+            return decimalFormat.format(0);
+
+        double result = 1;
+        double startPoint = 4;
+        for (int i = 1; i < n; i++)
+        {
+            result += 1 / startPoint;
+            startPoint += 3;
+        }
+
+        return decimalFormat.format(result);
     }
 
     @Override
