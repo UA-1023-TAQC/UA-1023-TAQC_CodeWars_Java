b4ed63c5a9f56196a788dcdc43cdfa464ef4f0e4
andriyav <andriyav@hotmail.com>
2023-06-15 21:05:19 +0300

    andriyav/newAvg

--- src/main/java/org/codewars/kata/implementation/andriyav/SevenImpl.java
+++ src/main/java/org/codewars/kata/implementation/andriyav/SevenImpl.java
@@ -4,7 +4,17 @@ import org.codewars.kata.Seven;
 
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sum = 0;
+        int n = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            n += 1;
+        }
+        double result = Math.ceil(navg * (n + 1) - sum);
+        if (result < 0) {
+            throw new IllegalArgumentException();
+        }
+        return (long) result;
     }
 
     public String seriesSum(int n) {
