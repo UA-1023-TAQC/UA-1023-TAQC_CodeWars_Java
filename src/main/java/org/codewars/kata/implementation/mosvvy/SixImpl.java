4c98b0fd0f9642f5ec48bd315d513a72bca17edc
Valeriia Moskalenko <moskalenkovvy@gmail.com>
2023-06-15 09:17:29 +0300

    add balance solution

--- src/main/java/org/codewars/kata/implementation/mosvvy/SixImpl.java
+++ src/main/java/org/codewars/kata/implementation/mosvvy/SixImpl.java
@@ -8,7 +8,22 @@ public class SixImpl implements Six {
     }
 
     public String balance(String book) {
-        return null;
+        StringBuilder result = new StringBuilder("");
+        String[] parseBook = book.replaceAll("[^a-zA-Z0-9\n.]", " ").split("\n");
+
+        double balance = Double.parseDouble(parseBook[0]);
+        double t = 0.0;
+        result.append(String.format("Original Balance: %.2f", balance));
+        for (int i = 1; i < parseBook.length; i++) {
+            String[] tmp = parseBook[i].split(" ", 3);
+            tmp[2] = tmp[2].replaceAll(" ", "");
+            t += Double.parseDouble(tmp[2]);
+            result.append(String.format("\\r\\n%s %s %s Balance %.2f", tmp[0], tmp[1], tmp[2], balance - t));
+        }
+        result.append(String.format("\\r\\nTotal expense  %.2f", t));
+        result.append(String.format("\\r\\nAverage expense  %.2f", t / (parseBook.length - 1)));
+
+        return String.valueOf(result);
     }
 
     public double f(double x) {
