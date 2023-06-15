9ea38b818da2f1d585a1a87f7ee1083efb2e8b11
Valeriia Moskalenko <moskalenkovvy@gmail.com>
2023-06-15 14:09:15 +0300

    remove newAvg solution from incorrect branch

--- src/main/java/org/codewars/kata/implementation/mosvvy/SevenImpl.java
+++ src/main/java/org/codewars/kata/implementation/mosvvy/SevenImpl.java
@@ -2,13 +2,9 @@ package org.codewars.kata.implementation.mosvvy;
 
 import org.codewars.kata.Seven;
 
-import java.util.Arrays;
-
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
-        double result = (navg * (arr.length+1) - Arrays.stream(arr).sum());
-        if (result <= 0) throw new IllegalArgumentException();
-        return (long) Math.ceil(result);
+        return 0;
     }
 
     public String seriesSum(int n) {
