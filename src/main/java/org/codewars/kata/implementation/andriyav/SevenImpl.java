21f20183a67aec207f078b1fc33fdc119e259de2
andriyav <andriyav@hotmail.com>
2023-06-15 21:12:42 +0300

    andriyav/seriesSum

--- src/main/java/org/codewars/kata/implementation/andriyav/SevenImpl.java
+++ src/main/java/org/codewars/kata/implementation/andriyav/SevenImpl.java
@@ -2,13 +2,25 @@ package org.codewars.kata.implementation.andriyav;
 
 import org.codewars.kata.Seven;
 
+import java.text.DecimalFormat;
+
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
         return 0;
     }
 
     public String seriesSum(int n) {
-        return null;
+        if(n==0) return "0.00";
+        DecimalFormat df = new DecimalFormat("#.00");
+        System.out.println(n);
+        int k = 1;
+        double index = 0;
+        for(int i = 0; i<n; i++){
+            index += (double) 1 /k;
+            k += 3;
+        }
+        String formattedNumber = df.format(index);
+        return formattedNumber;
     }
 
     @Override
