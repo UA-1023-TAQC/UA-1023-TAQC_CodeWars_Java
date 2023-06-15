9dcd5057fca098485230a3a97b6f5c060864634b
Valeriia Moskalenko <moskalenkovvy@gmail.com>
2023-06-15 14:06:42 +0300

    add newAvg solution

--- src/main/java/org/codewars/kata/implementation/mosvvy/SevenImpl.java
+++ src/main/java/org/codewars/kata/implementation/mosvvy/SevenImpl.java
@@ -2,9 +2,13 @@ package org.codewars.kata.implementation.mosvvy;
 
 import org.codewars.kata.Seven;
 
+import java.util.Arrays;
+
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double result = (navg * (arr.length+1) - Arrays.stream(arr).sum());
+        if (result <= 0) throw new IllegalArgumentException();
+        return (long) Math.ceil(result);
     }
 
     public String seriesSum(int n) {
