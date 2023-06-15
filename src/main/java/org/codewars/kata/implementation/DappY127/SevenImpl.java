1e3d7f97ec5cacf32ebebf4ac7b1c780b3b45d4f
Paul <paul.mshtk.707@gmail.com>
2023-06-15 11:51:43 +0300

    add sum of the first nth term of series kata solution

--- src/main/java/org/codewars/kata/implementation/DappY127/SevenImpl.java
+++ src/main/java/org/codewars/kata/implementation/DappY127/SevenImpl.java
@@ -2,13 +2,27 @@ package org.codewars.kata.implementation.DappY127;
 
 import org.codewars.kata.Seven;
 
+import java.text.DecimalFormat;
+
 public class SevenImpl implements Seven {
     public long newAvg(double[] arr, double navg) {
         return 0;
     }
 
     public String seriesSum(int n) {
-        return null;
+        if (n == 0) {
+            return "0.00";
+        }
+        double sum = 0;
+        double znamennik = 1;
+
+        for (int i = 0; i < n; i++) {
+            sum += 1 / znamennik;
+            znamennik += 3;
+        }
+
+        DecimalFormat df = new DecimalFormat("0.00");
+        return df.format(sum);
     }
 
     @Override
