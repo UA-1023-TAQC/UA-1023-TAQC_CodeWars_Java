61e2326c949192174cda71fc6e4b6cdc556a265f
Anastasiia <maschenkonastya@gmail.com>
2023-06-15 21:25:53 +0300

    Commit gap

--- src/main/java/org/codewars/kata/implementation/Anastasiia3795/FiveImpl.java
+++ src/main/java/org/codewars/kata/implementation/Anastasiia3795/FiveImpl.java
@@ -6,7 +6,28 @@ import java.math.BigInteger;
 
 public class FiveImpl implements Five {
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        long[] arr = new long[2];
+        int count;
+        long start, finish;
+        for(long i = m; i <= n; i++) {
+            count = 0;
+            if(BigInteger.valueOf(i).isProbablePrime((int)Math.log(Double.valueOf(i))) == true) {
+                start = Long.valueOf(i);
+                for(long j = i+1; j <= n; j++) {
+                    count++;
+                    if(BigInteger.valueOf(j).isProbablePrime((int)Math.log(Double.valueOf(String.valueOf(j)))) == true) {
+                        finish = Long.valueOf(j);
+                        if(count == g){
+                            arr[0] = start;
+                            arr[1] = finish;
+                            return arr;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     public int zeros(int n) {
