066075faf7a94c5f183b2d98617a7d21362feefc
Kovalchuk_Vita <vita.kovalchuk06@gmail.com>
2023-06-18 11:04:22 +0300

    with any solutions

--- src/main/java/org/codewars/kata/implementation/kovalchukvita/SevenImpl.java
+++ src/main/java/org/codewars/kata/implementation/kovalchukvita/SevenImpl.java
@@ -3,29 +3,16 @@ package org.codewars.kata.implementation.kovalchukvita;
 import org.codewars.kata.Seven;
 
 public class SevenImpl implements Seven {
-    public long newAvg(double[] arr, double navg) {
-        double sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
+        public long newAvg(double[] arr, double navg) {
+            return 0;
         }
-        double numDonations = arr.length + 1;
-        double expectedDonation = navg * numDonations - sum;
 
-        if (expectedDonation <= 0) {
-            throw new IllegalArgumentException();
+        public String seriesSum(int n) {
+            return null;
         }
-        return (long) Math.ceil(expectedDonation);
-    }
 
-    public String seriesSum(int n) {
-        double sum = 0d;
-        for (int i = 0; i < n; i++)
-            sum += 1d / (1 + 3 * i);
-        return String.format("%.2f", sum);
+        @Override
+        public int whereIsHe(int p, int bef, int aft) {
+            return 0;
+        }
     }
-
-    @Override
-    public int whereIsHe(int p, int bef, int aft) {
-        return 0;
-    }
-}
