39bc09be94c5cf8552cb1138538348eb7fcc3616
Kovalchuk_Vita <vita.kovalchuk06@gmail.com>
2023-06-18 22:00:38 +0300

    solution gap

--- src/main/java/org/codewars/kata/implementation/kovalchukvita/FiveImpl.java
+++ src/main/java/org/codewars/kata/implementation/kovalchukvita/FiveImpl.java
@@ -5,9 +5,27 @@ import org.codewars.kata.Five;
 import java.math.BigInteger;
 
 public class FiveImpl implements Five {
-    public long[] gap(int g, long m, long n) {
-        return new long[0];
+
+    public static boolean isPrime(long n)
+    {
+        for(int i=2;i<n/2;i++)
+        {
+            if(n%i==0) return false;
+        }
+        return true;
     }
+    public long[] gap(int g, long m, long n) {
+            long prev=0;
+            for(long i=m;i<=n;i++)
+            {
+                if(isPrime(i))
+                {
+                    if((i-prev)==g) return new long[]{prev,i};
+                    prev=i;
+                }
+            }
+            return null;
+        }
 
     public int zeros(int n) {
         return 0;
