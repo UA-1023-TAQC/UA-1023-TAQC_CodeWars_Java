38b4564245123530533629225556ea2a7b0ea2a7
Kovalchuk_Vita <vita.kovalchuk06@gmail.com>
2023-06-18 21:43:52 +0300

    solution mean variance

--- src/main/java/org/codewars/kata/implementation/kovalchukvita/SixImpl.java
+++ src/main/java/org/codewars/kata/implementation/kovalchukvita/SixImpl.java
@@ -16,11 +16,31 @@ public class SixImpl implements Six {
     }
 
     public double mean(String town, String strng) {
-        return 0;
+        if(!strng.contains(town+":")) return -1;
+        double[] v = doubleArr(town, strng);
+        double sum = 0;
+        for(int i=0; i<v.length; i++) sum += v[i];
+        return sum/v.length;
     }
-
     public double variance(String town, String strng) {
-        return 0;
+        if(!strng.contains(town+":")) return -1;
+        double[] v = doubleArr(town, strng);
+        double mean = mean(town, strng);
+        double temp = 0;
+        for(double a :v)  temp += (mean-a)*(mean-a);
+        return temp/v.length;
+    }
+    public double[] doubleArr(String town, String strng) {
+        String[] s = strng.split("\n");
+        for(int i=0; i<s.length; i++)
+            if(s[i].split(":")[0].equals(town)) {
+                String[] nm = s[i].split(":")[1].split(",");
+                double[] v = new double[nm.length];
+                for(int i2=0; i2<nm.length; i2++)
+                    v[i2] = Double.parseDouble(nm[i2].split(" ")[1]);
+                return v;
+            }
+        return null;
     }
 
     public String nbaCup(String resultSheet, String toFind) {
