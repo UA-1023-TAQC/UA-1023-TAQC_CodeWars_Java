07b593dc8fec372dc78d5f7dd82edb6bdb50d61e
Kovalchuk_Vita <vita.kovalchuk06@gmail.com>
2023-06-18 10:55:43 +0300

    change solution

--- src/main/java/org/codewars/kata/implementation/kovalchukvita/EightImpl.java
+++ src/main/java/org/codewars/kata/implementation/kovalchukvita/EightImpl.java
@@ -3,83 +3,39 @@ package org.codewars.kata.implementation.kovalchukvita;
 import org.codewars.kata.Eight;
 
 public class EightImpl implements Eight {
-    public int liters(double time) {
-        double liters = time * 0.5;
-        return (int) Math.floor(liters);
-    }
-
-    public double getVolumeOfCuboid(double length, double width, double height) {
-
-        return length*width*height;
-    }
-
-    public float mpgToKPM(float mpg) {
-        double kpl = (mpg / 4.54609188) * 1.609344;
-        return Math.round(kpl*100.0)/100.0f;
-    }
-
-    public int[] squareOrSquareRoot(int[] array) {
-        int [] result = new int [array.length];
-        for (int i = 0; i<array.length; i++) {
-            int squareRoot = (int) Math.sqrt(array[i]); //корінь квадратний
-            if (squareRoot * squareRoot == array[i]) {
-                result[i] = squareRoot;
-            } else {
-                result[i] = array[i] * array[i];
-            }
+        public int liters(double time) {
+            return 0;
         }
-        return result;
-    }
 
-    public int[] countPositivesSumNegatives(int[] input) {
-        if (input == null || input.length == 0) {
-            int[] emptyArray = {};
-            return emptyArray;
+        public double getVolumeOfCuboid(double length, double width, double height) {
+            return 0;
         }
-        int countOfPositive = 0;
-        int sumOfNegative = 0;
 
-        for (int i=0; i< input.length; i++) {
-            if ( input.length != 0 && input[i]>0) {
-                countOfPositive ++;
-            } else  if (input.length != 0 && input[i] < 0) {
-                sumOfNegative = sumOfNegative + input[i];
-            }
+        public float mpgToKPM(float mpg) {
+            return 0;
         }
-        int result [] = new int[] {countOfPositive, sumOfNegative};
-        return result; //return an array with count of positives and sum of negatives
-    }
 
-    public int stringToNumber(String str) {
-        // Convert str to a number
-        int number = Integer.parseInt(str);
-        return number;
-    }
-
-    public double TwoDecimalPlaces(double number) {
-        return Math.round(number*100d)/100d;
-    }
-
-    public int[] divisibleBy(int[] numbers, int divider) {
-        int counter = 0; // how many numbers divide for divider
-        for (int i = 0; i<numbers.length; i++) {
-            if (divider != 0 && numbers[i] % divider == 0 ) { //if condition true counter ++
-                counter++;
-            }
+        public int[] squareOrSquareRoot(int[] array) {
+            return new int[0];
         }
-        int[] result = new int[counter];
-        int index = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % divider == 0) {
-                result[index] = numbers[i];
-                index++;
-            }
-        }
-        return result;
-    }
 
-    @Override
-    public boolean am_i_wilson(double n) {
-        return false;
+        public int[] countPositivesSumNegatives(int[] input) {
+            return new int[0];
+        }
+
+        public int stringToNumber(String str) {
+            return 0;
+        }
+
+        public double TwoDecimalPlaces(double number) {
+            return 0;
+        }
+
+        public int[] divisibleBy(int[] numbers, int divider) {
+            return new int[0];
+        }
+
+        public boolean am_i_wilson(double n) {
+            return false;
+        }
     }
-}
