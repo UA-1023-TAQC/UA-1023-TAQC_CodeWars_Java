123a97cfa51b780f5b8c6cbee188f4f04d2051bb
Paul <paul.mshtk.707@gmail.com>
2023-06-19 09:08:33 +0300

    add ranking nba solution

--- src/main/java/org/codewars/kata/implementation/DappY127/SixImpl.java
+++ src/main/java/org/codewars/kata/implementation/DappY127/SixImpl.java
@@ -2,6 +2,8 @@ package org.codewars.kata.implementation.DappY127;
 
 import org.codewars.kata.Six;
 
+import static java.util.stream.Stream.of;
+
 public class SixImpl implements Six {
     public long findNb(long m) {
         return 0;
@@ -24,7 +26,42 @@ public class SixImpl implements Six {
     }
 
     public String nbaCup(String resultSheet, String toFind) {
-        return null;
+        if (toFind.isEmpty()) {
+            return "";
+        }
+
+        int[] stats = new int[5];
+        for (String match : of(resultSheet.split(",")).filter(s -> s.contains(toFind)).toArray(String[]::new)) {
+            if (match.contains(".")) {
+                return "Error(float number):" + match;
+            }
+            String[] teams = match.substring(0, match.lastIndexOf(' ')).replaceAll(" \\d+ ", "@").split("@");
+            if (teams[0].equals(toFind) || teams[1].equals(toFind)) {
+                int pointsA = Integer.parseInt(match.substring(match.lastIndexOf(' ') + 1));
+                int pointsB = Integer.parseInt(match.substring(teams[0].length() + 1, match.indexOf(teams[1]) - 1));
+                updateMatchStatistics(pointsA, pointsB, match.startsWith(toFind), stats);
+            }
+        }
+        return toFind + (stats[3] + stats[4] > 0 ? ":W=" + stats[0] + ";D=" + stats[2] + ";L=" + stats[1] + ";Scored=" + stats[3] + ";Conceded=" + stats[4] + ";Points=" + (3 * stats[0] + stats[2]) : ":This team didn't play!");
+    }
+
+    private static void updateMatchStatistics(int pointsA, int pointsB, boolean home, int[] stats) {
+        if (home) {
+            int temp = pointsA;
+            pointsA = pointsB;
+            pointsB = temp;
+        }
+
+        stats[3] += pointsA;
+        stats[4] += pointsB;
+
+        if (pointsA > pointsB) {
+            stats[0]++;
+        } else if (pointsA < pointsB) {
+            stats[1]++;
+        } else {
+            stats[2]++;
+        }
     }
 
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
