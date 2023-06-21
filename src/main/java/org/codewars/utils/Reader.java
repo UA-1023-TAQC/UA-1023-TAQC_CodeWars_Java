62c7a0b1c0e023f1478a8d9b999e4f890c517000
Anastasiia <maschenkonastya@gmail.com>
2023-06-21 16:05:17 +0300

    Reader readArrLong()

--- src/main/java/org/codewars/utils/Reader.java
+++ src/main/java/org/codewars/utils/Reader.java
@@ -1,8 +1,10 @@
 package org.codewars.utils;
 
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Reader {
+    private static final Scanner scanner = new Scanner(System.in);
     public static int readInt() {
         return 0;
     }
@@ -31,7 +33,24 @@ public class Reader {
         return new String[0];
     }
     public static long[] readArrLong() {
-        return new long[0];
+        while(true) {
+            System.out.print("Enter numbers, separated with space: ");
+            String input = scanner.nextLine();
+
+            if(input.isEmpty()) throw new IllegalArgumentException("Line is empty");
+
+            String[] numberStrings = input.split(" ");
+            long[] numbers = new long[numberStrings.length];
+            try {
+                for (int i = 0; i < numberStrings.length; i++) {
+                    numbers[i] = Long.parseLong(numberStrings[i]);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Your value is invalid\nTry again");
+                continue;
+            }
+            return numbers;
+        }
     }
     public static Boolean readBoolean() {
         return true;
