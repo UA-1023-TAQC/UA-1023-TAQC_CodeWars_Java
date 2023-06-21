e1e37ffde3f432dc3f1c2cfefe9077d793af8285
Anastasiia <maschenkonastya@gmail.com>
2023-06-21 14:03:48 +0300

    Reader readArrDouble()

--- src/main/java/org/codewars/utils/Reader.java
+++ src/main/java/org/codewars/utils/Reader.java
@@ -1,8 +1,10 @@
 package org.codewars.utils;
 
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Reader {
+    private static final Scanner scanner = new Scanner(System.in);
     public static int readInt() {
         return 0;
     }
@@ -25,7 +27,24 @@ public class Reader {
         return new int[0];
     }
     public static double[] readArrDouble() {
-        return new double[0];
+        while(true) {
+            System.out.print("Enter double numbers, separated with space: ");
+            String input = scanner.nextLine();
+
+            if(input.isEmpty()) throw new IllegalArgumentException("Line is empty");
+
+            String[] numberStrings = input.split(" ");
+            double[] numbers = new double[numberStrings.length];
+            try {
+                for (int i = 0; i < numberStrings.length; i++) {
+                    numbers[i] = Double.parseDouble(numberStrings[i]);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Your value is invalid\nTry again");
+                continue;
+            }
+            return numbers;
+        }
     }
     public static String[] readArrString() {
         return new String[0];
