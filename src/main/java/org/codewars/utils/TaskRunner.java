43d6c871f557344ba930bb867107f5b5833c190f
Natali-natali <rozdilska.n@gmail.com>
2023-06-21 18:38:04 +0300

    change

--- src/main/java/org/codewars/utils/TaskRunner.java
+++ src/main/java/org/codewars/utils/TaskRunner.java
@@ -39,7 +39,7 @@ public class TaskRunner {
         System.out.println("Input string with rainfall records");
         String str = Reader.readString();
         double result = user.getSixImpl().variance(town, str);
-        System.out.println("The variance of rainfall for the city of" + town + "is: " + result);
+        System.out.println("The variance of rainfall for the city of " + town + "is: " + result);
     }
 
     public static String runBalance(){
