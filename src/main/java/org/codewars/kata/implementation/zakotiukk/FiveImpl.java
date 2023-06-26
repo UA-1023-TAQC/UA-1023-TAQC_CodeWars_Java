dc45da633475c51b46cc89dadfadf09d695b2fc5
zakotiukk <zakotiukk@gmail.com>
2023-06-26 18:42:27 +0300

    add task

--- src/main/java/org/codewars/kata/implementation/zakotiukk/FiveImpl.java
+++ src/main/java/org/codewars/kata/implementation/zakotiukk/FiveImpl.java
@@ -14,7 +14,17 @@ public class FiveImpl implements Five {
     }
 
     public BigInteger perimeter(BigInteger n) {
-        return null;
+        BigInteger[] fib = new BigInteger[n.intValue() + 3];
+        fib[0] = BigInteger.ZERO;
+        fib[1] = BigInteger.ONE;
+        BigInteger sum = BigInteger.ZERO;
+        for (int i = 2; i <= n.intValue() + 2; i++) {
+            fib[i] = fib[i - 1].add(fib[i - 2]);
+        }
+        for (int i = 0; i <= n.intValue() + 1; i++) {
+            sum = sum.add(fib[i]);
+        }
+        return sum.multiply(BigInteger.valueOf(4));
     }
 
     public double solve(double m) {
