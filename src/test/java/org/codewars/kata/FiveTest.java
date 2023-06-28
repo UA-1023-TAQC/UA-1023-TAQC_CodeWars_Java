7f8f0d5df8433d75c0f631160082076dea1b0053
andriyav <andriyav@hotmail.com>
2023-06-28 23:49:25 +0300

    343_FiveTest_realise_testGap

--- src/test/java/org/codewars/kata/FiveTest.java
+++ src/test/java/org/codewars/kata/FiveTest.java
@@ -1,14 +1,26 @@
 package org.codewars.kata;
 
 import org.codewars.UserImplementations;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class FiveTest extends UserImplementations {
 
-    @Test
-    public void testGap() {
+    @DataProvider(name = "gapTestData")
+    private Object[][] gapTestData() {
+        Object[][] testData = new Object[][]{
+                {2, 100, 110, new long[]{101, 103}},
+                {4,100,110, new long[]{103, 107}},
+                {6,100,110, null},
+        };
+        return UserImplementations.combineImplWithTests(UserImplementations.FIVES, testData);
+    }
+    @Test (dataProvider = "gapTestData")
+    public void testGap(Five impl, int g, long m, long n, long[] expected) {
+        long[] actual = impl.gap(g, m, n);
+        assertEquals(actual, expected);
     }
 
     @Test
