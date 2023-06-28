0deeb19059c2571a7a590cc99aaba914bcf71145
Valeriia Moskalenko <moskalenkovvy@gmail.com>
2023-06-28 23:05:04 +0300

    322 [ReaderTest] testReadFloat realise (also Reader(Scanner) is added)

--- src/test/java/org/codewars/utils/ReaderTest.java
+++ src/test/java/org/codewars/utils/ReaderTest.java
@@ -1,6 +1,11 @@
 package org.codewars.utils;
 
+import org.codewars.UserImplementations;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 import static org.testng.Assert.*;
 
@@ -18,8 +23,23 @@ public class ReaderTest {
     public void testReadDouble() {
     }
 
-    @Test
-    public void testReadFloat() {
+    @DataProvider(name = "readFloatTestData")
+    private Object[][] readFloatTestData() {
+        Object[][] testData = new Object[][]{
+                {new Scanner("10"), 10f},
+                {new Scanner("0"), 0.0f},
+                {new Scanner("53,08"), 53.08f},
+                {new Scanner("-23453,9999999999"), -23453.9999999999f},
+                {new Scanner(",9"), 0.9f},
+                {new Scanner("53.08"), 53.08f}
+        };
+        return testData;
+    }
+    @Test(dataProvider = "readFloatTestData")
+    public void testReadFloat(Scanner scanner, float expected) {
+        Reader reader = new Reader(scanner);
+        float actual = reader.readFloat();
+        assertEquals(actual, expected);
     }
 
     @Test
