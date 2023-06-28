bbea52e277dd960aa0f141ea5878bcc1624c9bed
Volodumur Besarab <volodumur911@gmail.com>
2023-06-28 19:54:26 +0300

    add test FindNb

--- src/test/java/org/codewars/kata/SixTest.java
+++ src/test/java/org/codewars/kata/SixTest.java
@@ -1,14 +1,27 @@
 package org.codewars.kata;
 
 import org.codewars.UserImplementations;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class SixTest extends UserImplementations {
 
-    @Test
-    public void testFindNb() {
+    @DataProvider(name = "findNbTestData")
+    private Object[][] findNbTestData() {
+        Object[][] testData = new Object[][]{
+                {2022, 4183059834009L},
+                {-1, 24723578342962L},
+                {4824, 135440716410000L},
+                {3568, 40539911473216L}
+        };
+        return UserImplementations.combineImplWithTests(UserImplementations.SIXES, testData);
+    }
+    @Test(dataProvider = "findNbTestData")
+    public void testFindNb(Six impl, long expected, long value) {
+        long actual = impl.findNb(value);
+        assertEquals(actual,expected);
     }
 
     @Test
