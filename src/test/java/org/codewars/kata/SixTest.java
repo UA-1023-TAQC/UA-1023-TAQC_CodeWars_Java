f675039c8d5adc4327f2dea6c5b0d554890fbe61
Paul <paul.mshtk.707@gmail.com>
2023-06-29 11:19:16 +0300

    338 add help bookseler test

--- src/test/java/org/codewars/kata/SixTest.java
+++ src/test/java/org/codewars/kata/SixTest.java
@@ -2,6 +2,7 @@ package org.codewars.kata;
 
 import org.codewars.UserImplementations;
 import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
 
 import static org.testng.Assert.*;
 
@@ -31,7 +32,17 @@ public class SixTest extends UserImplementations {
     public void testNbaCup() {
     }
 
-    @Test
-    public void testStockSummary() {
+    @DataProvider(name = "stockSummaryTestData")
+    private Object[][] stockSummaryTestData() {
+        String[] art = new String[]{"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"};
+        String[] cd = new String[]{"A", "B"};
+        String expected = "(A : 200) - (B : 1140)";
+        Object[][] testData = new Object[][]{ {art, cd, expected}};
+        return UserImplementations.combineImplWithTests(UserImplementations.SIXES, testData);
+    }
+    @Test(dataProvider = "stockSummaryTestData")
+    public void testStockSummary(Six impl, String[] art, String[] cd, String expected) {
+        String actual = impl.stockSummary(art, cd);
+        assertEquals(actual, expected);
     }
 }
