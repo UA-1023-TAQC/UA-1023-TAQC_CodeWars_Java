782d38880a064adee513f4752b5f8c2b2eb02aaf
Liubomyr Halamaha <galamagal86@gmail.com>
2023-06-30 12:11:21 +0300

    addedexample test for Reader

--- src/test/java/org/codewars/utils/ReaderTest.java
+++ src/test/java/org/codewars/utils/ReaderTest.java
@@ -1,10 +1,25 @@
 package org.codewars.utils;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import java.io.*;
 
 public class ReaderTest {
+    private InputStream sysIn;
+
+    @BeforeMethod
+    public void setup() {
+        InputStream sysIn = System.in;
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        System.setIn(sysIn);
+
+    }
 
     @Test
     public void testReadInt() {
@@ -24,6 +39,25 @@ public class ReaderTest {
 
     @Test
     public void testReadLong() {
+        String input = "123456789";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        Reader reader = new Reader();
+        long result = reader.readLong();
+        Assert.assertEquals(result, 123456789);
+    }
+
+    @Test
+    public void testReadLongErr() {
+        String input = "1ggg56789\nw\n123456789";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        OutputStream out = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(out));
+
+        Reader reader = new Reader();
+        long result = reader.readLong();
+        Assert.assertEquals(out.toString().replace("\r", ""), "Your value is invalid. Try again\nYour value is invalid. Try again\n");
+        Assert.assertEquals(result, 123456789);
     }
 
     @Test
