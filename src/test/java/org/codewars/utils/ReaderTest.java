a05ece94bff45fd8c4bba571325bb2a61ddb2e70
Paul <paul.mshtk.707@gmail.com>
2023-06-30 07:56:00 +0300

    add some readLong tests

--- src/test/java/org/codewars/utils/ReaderTest.java
+++ src/test/java/org/codewars/utils/ReaderTest.java
@@ -2,6 +2,11 @@ package org.codewars.utils;
 
 import org.testng.annotations.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 import static org.testng.Assert.*;
 
 public class ReaderTest {
@@ -23,7 +28,33 @@ public class ReaderTest {
     }
 
     @Test
-    public void testReadLong() {
+    public void testReadLongWithValidInput() {
+        String input = "1234567890";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        Scanner scanner = new Scanner(inputStream);
+        Reader reader = new Reader(scanner);
+        long result = reader.readLong();
+        assertEquals(result, 1234567890L);
+    }
+
+    @Test//(expectedExceptions = NoSuchElementException.class)
+    public void testReadLongWithInvalidInput() {
+        String input = "qwertyqwerty";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        Scanner scanner = new Scanner(inputStream);
+        Reader reader = new Reader(scanner);
+        assertThrows(NoSuchElementException.class, () -> reader.readLong());
+    }
+
+    @Test
+    public void testReadLongWithInvalidThenValidInput() {
+        String input = "qwerty\n1234567890";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        Scanner scanner = new Scanner(inputStream);
+        Reader reader = new Reader(scanner);
+        //assertThrows(NoSuchElementException.class, () -> reader.readLong());
+        long result = reader.readLong();
+        assertEquals(result, 1234567890L);
     }
 
     @Test
