0e12d6bbc4f67f7cf7070c6169fdfacd99fb7c42
Kovalchuk_Vita <vita.kovalchuk06@gmail.com>
2023-06-30 11:48:58 +0300

    fix liters

--- src/main/java/org/codewars/kata/implementation/kovalchukvita/EightImpl.java
+++ src/main/java/org/codewars/kata/implementation/kovalchukvita/EightImpl.java
@@ -6,7 +6,7 @@ import org.codewars.kata.Eight;
 public class EightImpl extends Base implements Eight {
 
     public int liters(double time) {
-        return (int) Math.floor(time / 0.5);
+       return (int) (time * 0.5);
     }
 
     public double getVolumeOfCuboid(double length, double width, double height) {
