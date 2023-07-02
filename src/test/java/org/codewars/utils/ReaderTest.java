e790c60a398289746d7078b4e690f76fec9189a2
Francious <61871105+RostislavKrotenko@users.noreply.github.com>
2023-07-02 13:29:53 +0300

    remove report import

--- src/test/java/org/codewars/utils/ReaderTest.java
+++ src/test/java/org/codewars/utils/ReaderTest.java
@@ -2,8 +2,6 @@ package org.codewars.utils;
 
 import org.testng.Assert;
 
-import org.testng.Reporter;
-
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
