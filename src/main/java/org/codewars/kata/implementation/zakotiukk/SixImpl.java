3df8c29b5272b86dd22aae7fb9a9f6008c5f5cf9
zakotiukk <zakotiukk@gmail.com>
2023-07-06 17:12:10 +0300

    added stockSummary task

--- src/main/java/org/codewars/kata/implementation/zakotiukk/SixImpl.java
+++ src/main/java/org/codewars/kata/implementation/zakotiukk/SixImpl.java
@@ -1,5 +1,5 @@
 package org.codewars.kata.implementation.zakotiukk;
-
+import java.util.Arrays;
 import org.codewars.kata.Six;
 
 public class SixImpl implements Six {
@@ -37,6 +37,27 @@ public class SixImpl implements Six {
     }
 
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return null;
+        if (lstOfArt.length == 0 || lstOf1stLetter.length == 0) {
+            return "";
+        }
+        int[] categorySums = new int[lstOf1stLetter.length];
+        for (String art : lstOfArt) {
+            String[] splitArt = art.split(" ");
+            String code = splitArt[0];
+            int quantity = Integer.parseInt(splitArt[1]);
+            char category = code.charAt(0);
+            int categoryIndex = Arrays.asList(lstOf1stLetter).indexOf(Character.toString(category));
+            if (categoryIndex != -1) {
+                categorySums[categoryIndex] += quantity;
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < lstOf1stLetter.length; i++) {
+            result.append("(").append(lstOf1stLetter[i]).append(" : ").append(categorySums[i]).append(")");
+            if (i < lstOf1stLetter.length - 1) {
+                result.append(" - ");
+            }
+        }
+        return result.toString();
     }
 }
