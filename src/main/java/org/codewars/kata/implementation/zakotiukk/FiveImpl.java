9afce1b222fcc9af5bb02d2f51280d64d8593ba5
zakotiukk <zakotiukk@gmail.com>
2023-07-06 17:21:51 +0300

    added gap task

--- src/main/java/org/codewars/kata/implementation/zakotiukk/FiveImpl.java
+++ src/main/java/org/codewars/kata/implementation/zakotiukk/FiveImpl.java
@@ -6,7 +6,25 @@ import java.math.BigInteger;
 
 public class FiveImpl implements Five {
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        long[] prime = new long[2];
+        for (long i = m; i <= n; i++) {
+            boolean isPrime = true;
+            for (long j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                if (i - prime[0] == g) {
+                    prime[1] = i;
+                    return prime;
+                } else {
+                    prime[0] = i;
+                }
+            }
+        }
+        return null;
     }
 
     public int zeros(int n) {
